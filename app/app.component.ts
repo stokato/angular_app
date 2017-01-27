d01e8a2c34c941f7b70826208208c7ca4ca63d25
Ruslan Durov <s.t.o.k.a.t.p@gmail.com>
2017-01-27 15:29:20 +0300

    Работающий пример

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by "s.t.o.k.a.t.o" on 26.01.2017.
+ */
+
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'ponyracer-app',      // Заменяет этот селектор в html на компонент
+    template: '<h1>PonyRacer</h1>'  // Шаблон
+})
+export class PonyRacerAppComponent {
+
+}
---
+++ app/app.module.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by "s.t.o.k.a.t.o" on 26.01.2017.
+ */
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { PonyRacerAppComponent } from './app.component';
+
+@NgModule({
+    imports: [BrowserModule],
+    declarations: [PonyRacerAppComponent],
+    bootstrap: [PonyRacerAppComponent]
+})
+
+export class AppModule {
+
+}
